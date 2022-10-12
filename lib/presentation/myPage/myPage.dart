df282b716aaecfa7638be5948d0397dbfda0ade7
Keisuke Shimizu <keisukeshimizu.inoworl@gmail.com>
2022-10-13 07:20:30 +0900

    fix:マイページ作成用

---
+++ lib/presentation/myPage/myPage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class MyPage extends ConsumerWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        selectedItemColor: Colors.black,
+        currentIndex: 1,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Category',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ]
+      )
+    );
+  }
+}
---
+++ lib/presentation/myPage/myPage_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tarakite/infrastructure/authRepository.dart';
+import '../presentation_provider.dart';
+
+
