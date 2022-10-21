1012d7640847607c28f5d604a65561dda6018a3f
Keisuke Shimizu <keisukeshimizu.inoworl@gmail.com>
2022-10-22 06:52:42 +0900

    add:navigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import './presentation/signup/signup.dart';
-import './presentation/myPage/myPage.dart';
+import './presentation/bottom_navigation/bottom_navigation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -38,6 +38,6 @@ class App extends StatelessWidget {
             bodyText2: TextStyle(fontSize: 14, fontFamily: 'Hind'),
           ),
         ),
-        home: MyPage());
+        home: BottomNavigationPage());
   }
 }
