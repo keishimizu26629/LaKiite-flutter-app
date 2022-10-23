f7b2f941612da3026e74e295252e8947a86875ff
Keisuke Shimizu <keisukeshimizu.inoworl@gmail.com>
2022-10-23 11:24:21 +0900

    add:signinページ

---
+++ lib/presentation/signin/signin_view_model.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tarakite/infrastructure/authRepository.dart';
+import '../presentation_provider.dart';
+import '../../domain/interfaces/i_auth_repository.dart';
+
+final signInViewModelProvider = Provider<SignInViewModel>((ref) {
+  return SignInViewModel(
+    ref: ref,
+    authRepository: ref.watch(authRepositoryProvider),
+  );
+});
+
+class SignInViewModel {
+  final ProviderRef ref;
+  final IauthRepository authRepository;
+
+  SignInViewModel({required this.ref, required this.authRepository});
+
+  TextEditingController get emailAddressController =>
+      ref.read(emailAddressControllerStateProvider.state).state;
+
+  TextEditingController get passwordController =>
+      ref.read(passwordControllerStateProvider.state).state;
+
+  Future<void> signIn() async {
+    if (emailAddressController.text.isEmpty) {
+      throw 'メールアドレスを入力してください';
+    }
+    if (passwordController.text.isEmpty) {
+      throw 'パスワードを入力してください';
+    }
+    await authRepository.signUp(
+        email: emailAddressController.text, password: passwordController.text);
+    emailAddressController.text = '';
+    passwordController.text = '';
+
+  }
+}
