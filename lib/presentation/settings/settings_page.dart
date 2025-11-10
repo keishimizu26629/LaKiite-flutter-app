c7083dbd4b95bf85206003ce726afe9512714d91
keishimizu26629 <keisukeshimizu.inoworl@gmail.com>
2025-11-10 15:04:07 +0900

    fix: 未使用のurl_launcherインポートを削除

--- lib/presentation/settings/settings_page.dart
+++ lib/presentation/settings/settings_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:url_launcher/url_launcher.dart';
 import '../presentation_provider.dart';
 import 'edit_name_page.dart';
 import 'edit_email_page.dart';
