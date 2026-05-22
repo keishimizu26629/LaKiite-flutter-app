bc737d8499dc6cdb2c9a3d549e4fd115c81a8ffa
Musashi <70187407+keishimizu26629@users.noreply.github.com>
2026-05-23 06:54:14 +0900

    [codex] Simplify presentation provider exports (#154)
    * refactor: re-export application notifier providers
    
    * refactor: re-export auth state stream provider

--- lib/presentation/presentation_provider.dart
+++ lib/presentation/presentation_provider.dart
@@ -2,10 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lakiite/app/di/providers.dart';
 import 'package:lakiite/application/auth/auth_notifier.dart';
 import 'package:lakiite/application/auth/auth_state.dart';
-import 'package:lakiite/application/list/list_notifier.dart';
-import 'package:lakiite/application/list/list_state.dart';
-import 'package:lakiite/application/schedule/schedule_notifier.dart';
-import 'package:lakiite/application/schedule/schedule_state.dart';
 import 'package:lakiite/domain/entity/list.dart';
 import 'package:lakiite/domain/entity/user.dart';
 import '../domain/entity/schedule.dart';
@@ -14,28 +10,11 @@ export 'package:lakiite/app/di/providers.dart';
 export 'package:lakiite/application/notification/notification_notifier.dart'
     show currentUserIdProvider;
 export 'package:lakiite/application/auth/auth_notifier.dart'
-    show authNotifierProvider, authRepositoryProvider;
-
-/// 認証状態プロバイダー群
-// 認証状態の変更を監視するプロバイダー
-final authStateProvider = StreamProvider.autoDispose((ref) {
-  final authRepository = ref.watch(authRepositoryProvider);
-  return authRepository.authStateChanges();
-});
-
-/// スケジュール状態プロバイダー群
-// スケジュール状態を管理するNotifierプロバイダー
-final scheduleNotifierProvider =
-    AutoDisposeAsyncNotifierProvider<ScheduleNotifier, ScheduleState>(
-  ScheduleNotifier.new,
-);
-
-/// リスト状態プロバイダー群
-// リスト状態を管理するNotifierプロバイダー
-final listNotifierProvider =
-    AutoDisposeAsyncNotifierProvider<ListNotifier, ListState>(
-  ListNotifier.new,
-);
+    show authNotifierProvider, authRepositoryProvider, authStateStreamProvider;
+export 'package:lakiite/application/list/list_notifier.dart'
+    show listNotifierProvider;
+export 'package:lakiite/application/schedule/schedule_notifier.dart'
+    show scheduleNotifierProvider;
 
 /// 認証済みユーザーのリストを監視するStreamプロバイダー
 ///
